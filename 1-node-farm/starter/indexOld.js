e771564a5b0b24f17255ef0ec0b3f1e514e34f0e
hyangloyaltycom <hyang2@loyalty.com>
2019-10-10 11:52:04 -0400

    api

--- 1-node-farm/starter/indexOld.js
+++ 1-node-farm/starter/indexOld.js
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-// const slugify = require('slugify');
+//const slugify = require('slugify');
 // const replaceTemplate = require('./modules/replaceTemplate');
 
 /////////////////////////////////
@@ -21,7 +21,9 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
     console.log(data);
 });
 console.log('Will read file!');
-
+fs.readFile('./dev-data/data.json', 'utf-8', (err, data) => {
+    // console.log(data);
+});
 
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
     if (err) return console.log('ERROR! 💥');
@@ -55,6 +57,8 @@ console.log('Will read file!');
 //   'utf-8'
 // );
 
+//////////server5
+
 // const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 // const dataObj = JSON.parse(data);
 
@@ -63,52 +67,99 @@ console.log('Will read file!');
 
 // const server = http.createServer((req, res) => {
 //   const { query, pathname } = url.parse(req.url, true);
-
-//   // Overview page
-//   if (pathname === '/' || pathname === '/overview') {
-//     res.writeHead(200, {
-//       'Content-type': 'text/html'
-//     });
-
-//     const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-//     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
-//     res.end(output);
-
-//     // Product page
-//   } else if (pathname === '/product') {
-//     res.writeHead(200, {
-//       'Content-type': 'text/html'
-//     });
-//     const product = dataObj[query.id];
-//     const output = replaceTemplate(tempProduct, product);
-//     res.end(output);
-
-//     // API
-//   } else if (pathname === '/api') {
-//     res.writeHead(200, {
-//       'Content-type': 'application/json'
-//     });
-//     res.end(data);
-
-//     // Not found
-//   } else {
-//     res.writeHead(404, {
-//       'Content-type': 'text/html',
-//       'my-own-header': 'hello-world'
-//     });
-//     res.end('<h1>Page not found!</h1>');
-//   }
-// });
+//////////server4
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
-     console.log(req.url);
     const pathName = req.url;
+    // Overview page
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the OVERVIEW');
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+        res.end(output);
+
+        // Product page
     } else if (pathName === '/product') {
-        res.end('This is the PRODUCT');
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        });
+        const product = dataObj[query.id];
+        const output = replaceTemplate(tempProduct, product);
+        res.end(output);
+
+        // API
+    } else if (pathName === '/api') {
+        res.writeHead(200, {
+            'Content-type': 'application/json'
+        });
+        res.end(data);
+
+        // Not found
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
     }
-     res.end('Hello from the server');
 });
+/////server1
+// const server = http.createServer((req, res) => {
+//      console.log(req.url);
+//     const pathName = req.url;
+//     if (pathName === '/' || pathName === '/overview') {
+//         res.end('This is the OVERVIEW');
+//     } else if (pathName === '/product') {
+//         res.end('This is the PRODUCT');
+//     }
+//      res.end('Hello from the server');
+// });
+////////////server2
+// const server = http.createServer((req, res) => {
+//     console.log(req.url);
+//    const pathName = req.url;
+//    if (pathName === '/' || pathName === '/overview') {
+//        res.end('This is the OVERVIEW');
+//    } else if (pathName === '/product') {
+//        res.end('This is the PRODUCT');
+//    }else {
+//         res.writeHead(404, {
+//           'Content-type': 'text/html',
+//           'my-own-header': 'hello-world'
+//         });
+//         res.end('<h1>Page not found!</h1>');
+//       }
+// });
+//////////server3
+// const server = http.createServer((req, res) => {
+//     console.log(req.url);
+//     const pathName = req.url;
+//     if (pathName === '/' || pathName === '/overview') {
+//         res.end('This is the OVERVIEW');
+//     } else if (pathName === '/product') {
+//         res.end('This is the PRODUCT');
+//     } else if (pathName === '/api') {
+//         console.log('${__dirname}');
+//       fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+//         //  fs.readFile(`./dev-data/data.json`, 'utf-8', (err, data) => {
+//             const productData = JSON.parse(data);
+//             console.log(productData);
+//             res.writeHead(200, {'Content-type': 'application/json' });
+//             res.end(data);
+//         });
+//     }
+//     else {
+//         res.writeHead(404, {
+//             'Content-type': 'text/html',
+//             'my-own-header': 'hello-world'
+//         });
+//         res.end('<h1>Page not found!</h1>');
+//     }
+// });
 
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000');
