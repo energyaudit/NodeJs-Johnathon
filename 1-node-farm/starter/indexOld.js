5def3458c1e2beb4d08506986e087cb29a047118
hyangloyaltycom <hyang2@loyalty.com>
2020-02-11 16:39:14 -0500

    prettierrrc

--- 1-node-farm/starter/indexOld.js
+++ 1-node-farm/starter/indexOld.js
@@ -306,7 +306,50 @@ console.log('Will read file!');
 //     console.log('Listening to requests on port 8000');
 // });
 
-//////////server7
+// //////////server7
+
+// const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+// const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+// const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
+// const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+// //console.log(data);
+// const dataObj = JSON.parse(data);
+// console.log(slugify('Fresh Avocados',{lower:true}));
+// const server = http.createServer((req, res) => {
+//    const {query,pathname}=url.parse(req.url,true);//create object
+    
+//     //overview page
+   
+//     if (pathname === '/' || pathname === '/overview') {
+//         res.writeHead(200, { 'Content-type': 'text/html' });
+//         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');//not want array, use join into a big string
+//         const output=tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml)
+//         res.end(output);
+//     } else if (pathname === '/product') {
+//         console.log(query);
+//         res.writeHead(200, { 'Content-type': 'text/html' });
+//         const product=dataObj[query.id];//dataObj is array
+//         const output=replaceTemplate(tempProduct,product)
+//         res.end(output);
+//     } else if (pathname === '/api') {
+//         res.writeHead(200, { 'Content-type': 'application/json' });
+//         res.end(data);
+//     }
+//     else {
+//         res.writeHead(404, {
+//             'Content-type': 'text/html',
+//             'my-own-header': 'hello-world'
+//         });
+//         res.end('<h1>Page not found!</h1>');
+//     }
+// });
+
+// server.listen(8000, '127.0.0.1', () => {
+//     console.log('Listening to requests on port 8000');
+// });
+
+//////////server8
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
@@ -315,7 +358,9 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 //console.log(data);
 const dataObj = JSON.parse(data);
-console.log(slugify('Fresh Avocados',{lower:true}));
+//console.log(slugify('Fresh Avocados',{lower:true}));
+const slugs=dataObj.map(el=>slugify(el.productName,{lower:true}));
+console.log(slugs)
 const server = http.createServer((req, res) => {
    const {query,pathname}=url.parse(req.url,true);//create object
     
@@ -348,3 +393,4 @@ const server = http.createServer((req, res) => {
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000');
 });
+
